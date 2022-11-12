79e07b3e6b8c71571b32259870ee0e39d4f01afb
scttzwdrff <90075803+scttzwdrff@users.noreply.github.com>
2022-11-12 00:28:23 -0500

    polish release

--- main.js
+++ main.js
@@ -3,5 +3,4 @@ seedAutomata(auto,global_colors);
 seedFilter(row_filter);
 seedFilter(col_filter);
 plotAutomata(auto);
-//setInterval(apply_current_filter,delay);
 let interval = setInterval(perform_tiny_update,delay);
